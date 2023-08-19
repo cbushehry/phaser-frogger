2495bb276bbb5deb462b8f5e2e02cee700a5989b
c.bushehry <c.bushehry@gmail.com>
2023-08-19 00:03:58 -0700

    fixed game.js / player assets loads properly

--- js/game.js
+++ js/game.js
@@ -1,28 +1,29 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
-
+ 
 // load assets
 gameScene.preload = function(){
   // load images
-  this.load.image('background', 'assets/background.png');
-  this.load.image('player', 'assets/player.png');
+  this.load.image('background', 'https://jsbin-user-assets.s3.amazonaws.com/fariazz/original-background.png');
+  this.load.image('player', 'https://jsbin-user-assets.s3.amazonaws.com/fariazz/player.png');
 };
+ 
+
 
 // called once after the preload ends
 gameScene.create = function() {
   // create bg sprite
   let bg = this.add.sprite(0, 0, 'background');
-  // change the origin to the top-left corner
-  //bg.setOrigin(0,0);
+ 
   // place sprite in the center
   bg.setPosition(640/2, 360/2);
+ 
   let gameW = this.sys.game.config.width;
   let gameH = this.sys.game.config.height;
-  console.log(gameW, gameH);
-  console.log(bg);
-  console.log(this);
+  
+  let player = this.add.sprite(70, 180, 'player');
 };
-
+ 
 // set the configuration of the game
 let config = {
   type: Phaser.AUTO, // Phaser will use WebGL if available, if not it will use Canvas
@@ -30,6 +31,6 @@ let config = {
   height: 360,
   scene: gameScene
 };
-
+ 
 // create a new game, pass the configuration
 let game = new Phaser.Game(config);
