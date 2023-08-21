8be0c95c1fcf8cbd107661eddf9c258e0eef0c44
c.bushehry <c.bushehry@gmail.com>
2023-08-20 22:06:18 -0700

    minor irreleveant refactor

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
- 
+
 // load assets
 gameScene.preload = function(){
   // load images
@@ -8,31 +8,28 @@ gameScene.preload = function(){
   this.load.image('player', './assets/player.png');
   this.load.image('enemy', './assets/dragon.png');
 };
- 
+
 // called once after the preload ends
 gameScene.create = function() {
   // create bg sprite
   let bg = this.add.sprite(0, 0, 'background');
-  
- 
+
   // change the origin to the top-left corner
   bg.setOrigin(0, 0);
-  
-  // create the player
+
+  // create player
   let player = this.add.sprite(70, 180, 'player');
-  
-  // we are reducing the width by 50%, and we are doubling the height
   player.setScale(1, 1);
-  
-  // create an enemy
+
+  // create enemy1
   let enemy1 = this.add.sprite(250, 180, 'enemy');
   enemy1.flipX = true;
-  
-  //create a second enemy
+
+  //create enemy2
   let enemy2 = this.add.sprite(450, 180, 'enemy');
   enemy2.flipX = true;
 };
- 
+
 // set the configuration of the game
 let config = {
   type: Phaser.AUTO, // Phaser will use WebGL if available, if not it will use Canvas
@@ -40,6 +37,6 @@ let config = {
   height: 360,
   scene: gameScene
 };
- 
+
 // create a new game, pass the configuration
 let game = new Phaser.Game(config);
