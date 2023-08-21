ea01df29e66d971547ca1e386c445f2fe53313fa
c.bushehry <c.bushehry@gmail.com>
2023-08-20 21:57:46 -0700

    minor refctor, fixed positioning of player/enemy v2

--- js/game.js
+++ js/game.js
@@ -4,9 +4,9 @@ let gameScene = new Phaser.Scene('Game');
 // load assets
 gameScene.preload = function(){
   // load images
-  this.load.image('background', 'https://jsbin-user-assets.s3.amazonaws.com/fariazz/original-background.png');
-  this.load.image('player', 'https://jsbin-user-assets.s3.amazonaws.com/fariazz/player.png');
-  this.load.image('enemy', 'https://jsbin-user-assets.s3.amazonaws.com/fariazz/dragon.png');
+  this.load.image('background', './assets/background.png');
+  this.load.image('player', './assets/player.png');
+  this.load.image('enemy', './assets/dragon.png');
 };
  
 // called once after the preload ends
@@ -22,22 +22,15 @@ gameScene.create = function() {
   let player = this.add.sprite(70, 180, 'player');
   
   // we are reducing the width by 50%, and we are doubling the height
-  player.setScale(0.5, 2);
+  player.setScale(1, 1);
   
   // create an enemy
   let enemy1 = this.add.sprite(250, 180, 'enemy');
-  
-  enemy1.scaleX = 2;
-  enemy1.scaleY = 2;
+  enemy1.flipX = true;
   
   //create a second enemy
   let enemy2 = this.add.sprite(450, 180, 'enemy');
-  enemy2.displayWidth = 300;
-  
-  // flip
-  enemy1.flipX = true;
-  enemy1.flipY = true;
-  
+  enemy2.flipX = true;
 };
  
 // set the configuration of the game
