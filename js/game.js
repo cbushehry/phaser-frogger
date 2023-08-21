501647a8e081f061b98f7cd955fcdc341026bc42
c.bushehry <c.bushehry@gmail.com>
2023-08-20 21:53:06 -0700

    added enemies 1and2 v1

--- js/game.js
+++ js/game.js
@@ -4,24 +4,40 @@ let gameScene = new Phaser.Scene('Game');
 // load assets
 gameScene.preload = function(){
   // load images
-  this.load.image('background', './assets/background.png');
-  this.load.image('player', './assets/player.png');
+  this.load.image('background', 'https://jsbin-user-assets.s3.amazonaws.com/fariazz/original-background.png');
+  this.load.image('player', 'https://jsbin-user-assets.s3.amazonaws.com/fariazz/player.png');
+  this.load.image('enemy', 'https://jsbin-user-assets.s3.amazonaws.com/fariazz/dragon.png');
 };
  
-
-
 // called once after the preload ends
 gameScene.create = function() {
   // create bg sprite
   let bg = this.add.sprite(0, 0, 'background');
- 
-  // place sprite in the center
-  bg.setPosition(640/2, 360/2);
- 
-  let gameW = this.sys.game.config.width;
-  let gameH = this.sys.game.config.height;
   
+ 
+  // change the origin to the top-left corner
+  bg.setOrigin(0, 0);
+  
+  // create the player
   let player = this.add.sprite(70, 180, 'player');
+  
+  // we are reducing the width by 50%, and we are doubling the height
+  player.setScale(0.5, 2);
+  
+  // create an enemy
+  let enemy1 = this.add.sprite(250, 180, 'enemy');
+  
+  enemy1.scaleX = 2;
+  enemy1.scaleY = 2;
+  
+  //create a second enemy
+  let enemy2 = this.add.sprite(450, 180, 'enemy');
+  enemy2.displayWidth = 300;
+  
+  // flip
+  enemy1.flipX = true;
+  enemy1.flipY = true;
+  
 };
  
 // set the configuration of the game
