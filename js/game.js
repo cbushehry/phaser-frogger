990cfc957f75692d401e0bd497a22fbaae62907b
c.bushehry <c.bushehry@gmail.com>
2023-08-21 21:04:24 -0700

    enemy1 bounces up and down

--- js/game.js
+++ js/game.js
@@ -1,53 +1,82 @@
+// create a new scene
 let gameScene = new Phaser.Scene('Game');
-
-//initiate game scene parameters
+// initiate scene parameters
 gameScene.init = function() {
+  // player speed
   this.playerSpeed = 1;
+  // enemy speed
+  this.enemyMinSpeed = 1;
+  this.enemyMaxSpeed = 2;
+  // boundaries
+  this.enemyMinY = 80;
+  this.enemyMaxY = 280;
 };
-
 // load assets
 gameScene.preload = function(){
   // load images
-  this.load.image('background', './assets/background.png');
-  this.load.image('player', './assets/player.png');
-  this.load.image('enemy', './assets/dragon.png');
-  this.load.image('goal', './assets/treasure.png');
+  this.load.image('background', 'assets/background.png');
+  this.load.image('player', 'assets/player.png');
+  this.load.image('enemy', 'assets/dragon.png');
+  this.load.image('goal', 'assets/treasure.png');
 };
 
 // called once after the preload ends
 gameScene.create = function() {
+
   // create bg sprite
   let bg = this.add.sprite(0, 0, 'background');
-
   // change the origin to the top-left corner
-  bg.setOrigin(0, 0);
+  bg.setOrigin(0,0);
 
-  // create player
-  this.player = this.add.sprite(70, 180, 'player');
-  this.player.setScale(0.5, 0.5);
+  // create the player
+  this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
+  // we are reducing the width and height by 50%
+  this.player.setScale(0.5);
 
-  this.goal = this.add.sprite(this.sys.game.config.width - 80,this.sys.game.config.height / 2, 'goal')
+  // goal
+  this.goal = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'goal');
   this.goal.setScale(0.6);
+
+  // enemy
+  this.enemy = this.add.sprite(120, this.sys.game.config.height / 2, 'enemy');
+  this.enemy.flipX = true;
+  this.enemy.setScale(0.6);
+  // set enemy speed
+  let dir = Math.random() < 0.5 ? 1 : -1;
+  let speed = this.enemyMinSpeed + Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
+  this.enemy.speed = dir * speed;
+
 };
 
-gameScene.update = function() {
-  //checkfor active input
+// this is called up to 60 times per second
+gameScene.update = function(){
+  // check for active input (left click / touch)
   if(this.input.activePointer.isDown) {
+    // player walks
     this.player.x += this.playerSpeed;
   }
 
   // treasure overlap check
   let playerRect = this.player.getBounds();
   let treasureRect = this.goal.getBounds();
-  
+
   if(Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)) {
     console.log('reached goal!');
-    
     // restart the Scene
     this.scene.restart();
     return;
   }
-}
+
+  // enemy movement
+  this.enemy.y += this.enemy.speed;
+  // check we haven't passed min or max Y
+  let conditionUp = this.enemy.speed < 0 && this.enemy.y <= this.enemyMinY;
+  let conditionDown = this.enemy.speed > 0 && this.enemy.y >= this.enemyMaxY;
+  // if we passed the upper or lower limit, reverse
+  if(conditionUp || conditionDown) {
+    this.enemy.speed *= -1;
+  }
+};
 
 // set the configuration of the game
 let config = {
